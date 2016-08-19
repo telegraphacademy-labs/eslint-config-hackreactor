3ade0716c3e02c01f8adfa4560fea4d069c9a1ba
Josh Wyatt <joshwyatt@users.noreply.github.com>
2016-08-18 17:15:10 -0700

    Update to only give warning on function declaration

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ module.exports = {
     /* Language constructs */
     'curly': 2,
     'eqeqeq': [2, 'smart'],
-    'func-style': [2, 'expression'],
+    'func-style': [1, 'expression'],
     /* Semicolons */
     'semi': 2,
     'no-extra-semi': 2,
